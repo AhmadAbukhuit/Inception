3ed8c7753d841d976bd46ad6fdc605742df27e6b
Ahmad Naji <ahmadnajikam@gmail.com>
2026-08-11 17:45:43 +0300

    feat: stash most of mariadb implmentation

--- srcs/requirements/mariadb/tools/main.sh
+++ srcs/requirements/mariadb/tools/main.sh
@@ -1,17 +1,30 @@
-set -e 
+#!/bin/sh
+set -eu 
 
-if [[ ! -d "$DATADIR/mysql" ]]; then
-	mariadb-install-db --user=mysql \ 
-		--datadir=/var/lib/mysql \
+if [ ! -d "$DATADIR/mysql" ]; then
+	echo "setup the mariadb data directory"
+	mariadb-install-db --user=mysql \
+		--datadir="$DATADIR" \
 		--skip-test-db
+
+	echo "running the mariadbd...."
+	mariadbd \
+	--user=mysql \
+	--datadir="$DATADIR" \
+	--bootstrap <<EOF
+CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+
+CREATE USER IF NOT EXISTS '${DB_USER}'@'%'
+    IDENTIFIED BY '${DB_PASSWORD}';
+
+GRANT ALL PRIVILEGES
+    ON ${DB_NAME}.*
+    TO '${DB_USER}'@'%';
+EOF
 fi
 
-mariadbd \
+exec mariadbd \
+    --user=mysql \
     --datadir="$DATADIR" \
-    --socket="$SOCKET" \
-    --skip-networking
-
-
-exec mariadbd
-
-
+    --bind-address=0.0.0.0 \
+    --port=3306
